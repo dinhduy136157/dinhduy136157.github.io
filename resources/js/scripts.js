046efd4a04d51d04a73d7fe9b469810b0f0db381
dinhduy136157 <96122289+dinhduy136157@users.noreply.github.com>
2021-12-14 21:14:30 +0700

    Add files via upload

---
+++ resources/js/scripts.js
@@ -0,0 +1,16 @@
+// $(document).ready(
+//     function(){
+//         $('.about-section').waypoint(
+//             function(direction){
+//                 if(direction == "down"){
+//                     $('nav').addClass('sticky');
+//                 }else{
+//                     $('nav').removeClass('sticky')
+
+//                 }
+//             },{
+//                 offset: '600px'
+//             }
+//         )
+//     }
+// )
